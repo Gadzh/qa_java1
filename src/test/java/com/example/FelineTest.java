5170aba9654bab39d7d84a62519dd479c33ad7c1
Gadzh <gadzh955@yandex.ru>
2023-01-09 15:43:50 +0300

    2

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -1,4 +1,36 @@
-import static org.junit.Assert.*;
+package com.example;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class FelineTest {
-  
+
+
+    @Test
+    public void getKittens() {
+
+        Feline feline = new Feline();
+        assertEquals(1, feline.getKittens());
+    }
+
+    @Test
+    public void eatMeat() throws Exception {
+        Feline feline = new Feline();
+        assertEquals(List.of("Животные", "Птицы", "Рыба"), feline.eatMeat());
+    }
+
+    @Test
+    public void getFamily() {
+        Feline feline = new Feline();
+        assertEquals("Кошачьи", feline.getFamily());
+    }
+
+    @Test
+    public void testGetKittens() {
+        Feline feline = new Feline();
+        assertEquals(1, feline.getKittens());
+    }
 }
