2acdf65e67cca2dc1e4a57727a2fd3dec705f0ee
Gadzh <gadzh955@yandex.ru>
2023-01-10 09:12:11 +0300

    3

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -1,20 +1,14 @@
 package com.example;
 
 import org.junit.Test;
-
 import java.util.List;
-
 import static org.junit.Assert.*;
-
 public class CatTest {
-
     @Test
     public void getSound() {
         Cat cat = new Cat(new Feline());
         assertEquals("Мяу", cat.getSound());
-
     }
-
     @Test
     public void getFood() throws Exception {
         Cat cat = new Cat(new Feline());
